4faaca4654f7f8ebdaeafca8256aeff15e54f672
Yoandy M <yoandym@gmail.com>
2016-11-11 00:23:07 -0300

    valor del dolar segun multicaja e incorporando comisiones de paypal y multicaja en la conversionn de moneda

--- paypal/currencies.py
+++ paypal/currencies.py
@@ -10,6 +10,10 @@ class Currency(object):
         new_amount = -1
         try:
             new_amount = amount / settings.CURRENCIES[from_curr][to_curr]
+            new_amount += settings.MULTICAJA_FEE
+
+            paypal_fee = new_amount * settings.PAYPAL_FEE / 100
+            new_amount += paypal_fee
         except KeyError:
             raise Exception("Conversion from %s to %s is not available")
 
