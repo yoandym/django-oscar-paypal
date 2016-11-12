dbf30abc895be6b4486a0995bdb806216bd6bf98
Yoandy M <yoandym@gmail.com>
2016-11-12 08:38:37 -0300

    trabajo con decimal

--- paypal/settings.py
+++ paypal/settings.py
@@ -1,4 +1,5 @@
+from decimal import  Decimal as D
 
-CURRENCIES = {'CLP': {'USD': 631}}
-MULTICAJA_FEE = 10
-PAYPAL_FEE = 5.4
+CURRENCIES = {'CLP': {'USD': D(631)}}
+MULTICAJA_FEE = D(10)
+PAYPAL_FEE = D(5.4)
